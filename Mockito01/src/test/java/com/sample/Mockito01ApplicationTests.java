a18d578bf1fd25498b9cb37a25fc007200db308f
Manonmani13 <manonmani.pandukumar@gmail.com>
2024-11-03 15:58:32 +0530

    mockits

--- Mockito01/src/test/java/com/sample/Mockito01ApplicationTests.java
+++ Mockito01/src/test/java/com/sample/Mockito01ApplicationTests.java
@@ -1,4 +1,5 @@
 package com.sample;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -7,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.powermock.api.mockito.PowerMockito;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 @ExtendWith(MockitoExtension.class)
@@ -28,6 +30,14 @@ class Mockito01ApplicationTests {
         when(mockDao.findNameById(101)).thenReturn("Nick");
         String name = service.getNameByUserId(101);
         assertEquals("Nick", name);
+    }
+    @Test
+    void testGetEmailByUserId() {
+    	UserDaoImpl mockDao =PowerMockito.mock(UserDaoImpl.class);
+        PowerMockito.when(mockDao.findEmailById(101)).thenCallRealMethod();
+        service=new UserService(mockDao);
+        String email = service.getEmailByUserId(101);
+        assertEquals("john.k@gmail.com", email);
     }
 }
 
